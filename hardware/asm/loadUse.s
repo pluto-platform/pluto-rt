d05d8e5884f92e03daeebadd348ced1818522cb7
Tjark Petersen <tjark-petersen@gmx.de>
2022-03-13 13:21:53 +0100

    remove non rust projects

--- hardware/asm/loadUse.s
+++
@@ -1,9 +0,0 @@
-nop
-lw x1, 128(x0)
-addi x2, x1, 8
-nop
-nop
-nop
-nop
-ecall
-
--- hardware/synthesis/quartus/pinout.tcl
+++
@@ -1,3 +0,0 @@
-set_location_assignment PIN_Y2 -to clock
-set_location_assignment PIN_G19 -to io_led
-set_location_assignment PIN_AB28 -to reset
