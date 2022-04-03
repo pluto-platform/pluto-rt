f9b89ad944b877ee1d6e713d4a422171d1b1a7e5
Tjark Petersen <tjark-petersen@gmx.de>
2022-04-03 15:49:57 +0200

    update to use riscv-rt

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,15 @@
 #![no_std]
 #![no_main]
 
+extern crate panic_halt;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+use core::arch::asm;
+use riscv_rt::entry;
+
+#[entry]
+fn main() -> ! {
     let raw = 0x10000 as *mut u8;
-    let mut state = 0x00u8;
-    
-    loop {
-        delay();
-        unsafe{ core::ptr::write_volatile(raw, state) }
-        state = !state;
-    }
-}
-
-fn delay() {
-    for _ in 0..10000 {
-        unsafe{ core::ptr::write_volatile(0 as *mut u8, 0) }
-    }
-}
-
-use core::panic::PanicInfo;
-
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    unsafe{ core::ptr::write_volatile(raw, 0xFF) }
+    unsafe { asm!("ecall"); }
+    loop{}
 }
